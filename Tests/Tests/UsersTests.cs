922e856f6f0a930c8e2ea606cfa01a381dcbd28e
g3tf13ld <pasha.fedortzov@yandex.ru>
2022-06-13 01:27:34 +0300

    Add user tests

--- Tests/Tests/UsersTests.cs
+++ Tests/Tests/UsersTests.cs
@@ -1,8 +1,8 @@
 ﻿using System.Net;
-using Business.Models.API;
+using Business.ApiServices;
 using Business.Models.Business;
+using Core;
 using NUnit.Framework;
-using RestSharp;
 using Tests.Base;
 
 namespace Tests.Tests
@@ -10,24 +10,63 @@ namespace Tests.Tests
     public class UsersTests : BaseTest
     {
         [Test]
-        public void Test1()
+        [Regression]
+        public void GetUsersList_ShouldBeOk()
         {
-            var req = new RestRequest("users", Method.Get);
-            var resp = Client.ExecuteAsync<BaseResponseWithListModel<User>>(req).Result;
-            var content = resp.Data;
-            // var content2 = JsonConvert.DeserializeObject<BaseResponseWithListModel<User>>();
-            var a = 0;
+            var response = new UsersService(Client).GetUsersList();
             
             Assert.Multiple(() =>
             {
-                Assert.AreEqual(HttpStatusCode.OK, resp.StatusCode);
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Data);
+                Assert.Less(0, response.Data.Page);
+                Assert.Less(0, response.Data.Total);
+                Assert.LessOrEqual(0, response.Data.PerPage);
+                Assert.Less(0, response.Data.Data.Length);
+                
+                CheckSupportModel(response.Data.Support);
             });
         }
 
         [Test]
-        public void UsersTest()
+        public void SingleUser_ShouldBeOk()
         {
+            var response = new UsersService(Client).GetUserById("2");
             
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Data);
+                Assert.NotNull(response.Data.Data.Avatar);
+                Assert.NotNull(response.Data.Data.Email);
+                Assert.NotNull(response.Data.Data.FirstName);
+                Assert.NotNull(response.Data.Data.LastName);
+                Assert.LessOrEqual(0, response.Data.Data.Id);
+
+                CheckSupportModel(response.Data.Support);
+            });
+        }
+        
+        [Test]
+        public void SingleUser_ShouldBeNotFound()
+        {
+            var response = new UsersService(Client).GetUserById("23");
+            
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+                Assert.True(response.Content == "{}");
+            });
+        }
+
+        private void CheckSupportModel(SupportModel supportModel)
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual("https://reqres.in/#support-heading", supportModel.Text);
+                Assert.AreEqual("To keep ReqRes free, contributions towards server costs are appreciated!",
+                    supportModel.Url);
+            });
         }
     }
 }
