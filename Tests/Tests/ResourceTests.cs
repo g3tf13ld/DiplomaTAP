833db06c0f1a7a23d3c104058ff09e6096b81923
g3tf13ld <pasha.fedortzov@yandex.ru>
2022-06-16 00:20:31 +0300

    Add new tests

---
+++ Tests/Tests/ResourceTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using Business.ApiServices;
+using Core;
+using NUnit.Framework;
+using Tests.Base;
+
+namespace Tests.Tests
+{
+    public class ResourceTests: BaseTest
+    {
+        [Test]
+        [Regression]
+        public void GetResourcesList_ShouldBeOk()
+        {
+            var response = new ResourceService(Client).GetResourcesList();
+            
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Data);
+                Assert.NotNull(response.Data);
+                Assert.Less(0, response.Data.Page);
+                Assert.Less(0, response.Data.Total);
+                Assert.LessOrEqual(0, response.Data.PerPage);
+                Assert.Less(0, response.Data.Data.Length);
+
+                CheckSupportModel(response.Data.Support);
+            });
+        }
+
+        [Test]
+        public void GetResourceById_ShouldBeOk()
+        {
+            var response = new ResourceService(Client).GetResourceById("2");
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Data);
+                Assert.NotNull(response.Data.Data.Color);
+                Assert.NotNull(response.Data.Data.PantoneValue);
+                Assert.NotNull(response.Data.Data.Name);
+                Assert.NotNull(response.Data.Data.Year);
+                Assert.LessOrEqual(0, response.Data.Data.Id);
+
+                CheckSupportModel(response.Data.Support);
+            });
+        }
+        
+        [Test]
+        public void GetResourceById_ShouldBeNotFound()
+        {
+            var response = new ResourceService(Client).GetResourceById("23");
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+                Assert.True(response.Content == "{}");
+            });
+        }
+    }
+}
--- Tests/Tests/UsersTests.cs
+++ Tests/Tests/UsersTests.cs
@@ -59,13 +59,82 @@ namespace Tests.Tests
             });
         }
 
-        private void CheckSupportModel(SupportModel supportModel)
+        [Test]
+        public void CreateUser_ShouldBeCreated()
         {
+            var name = "Pavel";
+            var job = "AQA Engineer";
+
+            var response = new UsersService(Client).CreateUser(new UserModel()
+            {
+                Name = name,
+                Job = job
+            });
+
             Assert.Multiple(() =>
             {
-                Assert.AreEqual("https://reqres.in/#support-heading", supportModel.Text);
-                Assert.AreEqual("To keep ReqRes free, contributions towards server costs are appreciated!",
-                    supportModel.Url);
+                Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+                Assert.NotNull(response.Data);
+                Assert.AreEqual(job, response.Data.Job);
+                Assert.AreEqual(name, response.Data.Name);
+                Assert.NotNull(response.Data.Id);
+                Assert.NotNull(response.Data.CreatedAt);
+            });
+        }
+
+        [Test]
+        public void PutUpdateUser_ShouldBeOk()
+        {
+            var name = "morpheus";
+            var job = "AQA Engineer";
+
+            var response = new UsersService(Client).UpdateUserPutRequest("2", new UserModel()
+            {
+                Name = name,
+                Job = job
+            });
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Data);
+                Assert.AreEqual(job, response.Data.Job);
+                Assert.AreEqual(name, response.Data.Name);
+                Assert.NotNull(response.Data.UpdatedAt);
+            });
+        }
+
+        [Test]
+        public void PatchUpdateUser_ShouldBeOk()
+        {
+            var name = "morpheus";
+            var job = "AQA Engineer";
+
+            var response = new UsersService(Client).UpdateUserPutRequest("2", new UserModel()
+            {
+                Name = name,
+                Job = job
+            });
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                Assert.NotNull(response.Data);
+                Assert.AreEqual(job, response.Data.Job);
+                Assert.AreEqual(name, response.Data.Name);
+                Assert.NotNull(response.Data.UpdatedAt);
+            });
+        }
+
+        [Test]
+        public void DeleteUser_ShouldBeNoContent()
+        {
+            var response = new UsersService(Client).DeleteUserById("2");
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+                Assert.IsEmpty(response.Content);
             });
         }
     }
